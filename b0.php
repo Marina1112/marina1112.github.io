d9f1167b82e5bc09999f6a53ab9007779772a510
marina1112 <divina_marinka@mail.ru>
2018-05-13 15:48:11 +0300

    int.commit

---
+++ b0.php
@@ -0,0 +1,34 @@
+<meta charset="utf-8"> 
+<?php
+$urok="Урок 22";
+error_reporting( E_ERROR );   //Отключение предупреждений и нотайсов (warning и notice) на сайте
+// создание переменных из полей формы		
+if (isset($_POST['name']))			{$name			= $_POST['name'];		if ($name == '')	{unset($name);}}
+if (isset($_POST['email']))		{$email		= $_POST['email'];		if ($email == '')	{unset($email);}}
+if (isset($_POST['text']))			{$text			= $_POST['text'];		if ($text == '')	{unset($text);}}
+if (isset($_POST['sab']))			{$sab			= $_POST['sab'];		if ($sab == '')		{unset($sab);}}
+//стирание треугольных скобок из полей формы
+if (isset($name) ) {
+$name=stripslashes($name);
+$name=htmlspecialchars($name);
+}
+if (isset($emai) ) {
+$email=stripslashes($email);
+$email=htmlspecialchars($email);
+}
+if (isset($text) ) {
+$text=stripslashes($text);
+$text=htmlspecialchars($text);
+}
+// адрес почты куда придет письмо
+$address="mar_re96@mail.ru";
+// текст письма 
+$note_text="Тема : $urok \r\nИмя : $name \r\n Email : $email \r\n Дополнительная информация : $text";
+
+if (isset($name)  &&  isset ($sab) ) {
+mail($address,$urok,$note_text,"Content-type:text/plain; windows-1251"); 
+// сообщение после отправки формы
+echo "<p style='color:#009900;'>Уважаемый(ая) <b>$name</b> Ваше письмо отправленно успешно. <br> Спасибо. <br>Вам скоро ответят на почту <b> $email</b>.</p>";
+}
+
+?>
